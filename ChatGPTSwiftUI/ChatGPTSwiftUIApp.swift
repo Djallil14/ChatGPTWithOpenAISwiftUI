b35b2f34cf6812a23ada9755eaa25096c6ffbe9d
Djallil Elkebir <elkebir.djallil@icloud.com>
2023-02-16 15:08:41 -0500

    Initial Commit

---
+++ ChatGPTSwiftUI/ChatGPTSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTSwiftUIApp.swift
+//  ChatGPTSwiftUI
+//
+//  Created by Djallil on 2023-02-16.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
